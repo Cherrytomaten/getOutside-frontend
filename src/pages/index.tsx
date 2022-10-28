ead38429904ab17c19b49494f47ed1097fe4b926
MarlonAACN <107611839+MarlonAACN@users.noreply.github.com>
2022-10-28 13:36:22 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center py-10 bg-black'>
+      <h1 className='text-7xl font-bold text-teal-500'>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Home;
