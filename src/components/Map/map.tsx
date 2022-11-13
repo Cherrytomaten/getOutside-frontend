833854dddff023a55f94bc3c6550d25255a47556
Adham <adham.elgendy@aconso.com>
2022-11-13 14:37:47 +0100

    get data from json pins

--- src/components/Map/map.tsx
+++ src/components/Map/map.tsx
@@ -1,28 +1,60 @@
 import 'leaflet/dist/leaflet.css';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import style from '../../styles/Home.module.css';
-// import * as parkData from '../data/pins.json';
 import { Icon } from 'leaflet';
-// import { Link } from 'theme-ui';
-import Link from 'next/link';
+import * as pinsData from '../../data/pins.json';
 
+type PinProps = {
+  type: string;
+  properties: {
+    PARK_ID: number;
+    FACILITYID: number;
+    NAME: string;
+    TYPE: string;
+    ADDRESS: string;
+    OPEN: null;
+    NOTES: string;
+    DESCRIPTION: string;
+    PICTURE: null;
+  };
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+};
 const position = [52.520008, 13.404954];
 const position2 = [52.480894, 13.355937];
 const position3 = [52.494056, 13.442307];
+const position4 = [52.520057, 13.406163];
 export const icon = new Icon({
   iconUrl: '/pin.png',
   iconSize: [70, 70],
 });
 
-// function pinClicked() {
-//   console.log('click');
-// }
-
-// function pinCosed() {
-//   console.log('close');
-// }
-
 function Map() {
+  const [activePin, setActivePin] = React.useState(null);
+  // const [activePin, setActivePin] =
+  //   useState <
+  //   PinProps >
+  //   {
+  //     type: '';
+  //     properties: {
+  //       PARK_ID: 0;
+  //       FACILITYID: 0;
+  //       NAME: '';
+  //       TYPE: '';
+  //       ADDRESS: '';
+  //       OPEN: null;
+  //       NOTES: '';
+  //       DESCRIPTION: '';
+  //       PICTURE: null;
+  //     },
+  //     geometry: {
+  //       type: '';
+  //       coordinates: [];
+  //     },
+  //   };
   return (
     <MapContainer
       className={style.map}
@@ -34,22 +66,37 @@ function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={icon}>
-        <Popup>
-          Skatepark bla bla bla. <br /> Easily customizable.
+      {pinsData.features.map((pin) => (
+        <Marker
+          onClick={() => {
+            // which parks is set as active
+            setActivePin(pin);
+            console.log('click pin: ', pin);
+          }}
+          onClose={() => {
+            setActivePin(null);
+            console.log('null');
+          }}
+          key={pin.properties.PARK_ID}
+          position={[pin.geometry.coordinates[0], pin.geometry.coordinates[1]]}
+          icon={icon}
+        >
+          <Popup>
+            <div>
+              <h2>{pin.properties.NAME}</h2>
+              <p>{pin.properties.DESCRIPTION}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+      {activePin && (
+        <Popup position={[52.520008, 13.404954]}>
+          <div>
+            <h2>{activePin.properties.NAME}</h2>
+            <p>{activePin.properties.DESCRIPTION}</p>
+          </div>
         </Popup>
-      </Marker>
-      <Marker position={position2} icon={icon}>
-        <Popup>
-          Basketball blo blo bla. <br /> Easily customizable.
-          <Link href="/maps/baskteball">Baskteball at 6</Link>
-        </Popup>
-      </Marker>
-      <Marker position={position3} icon={icon}>
-        <Popup>
-          Spa bli bli blo. <br /> nix
-        </Popup>
-      </Marker>
+      )}
     </MapContainer>
   );
 }
