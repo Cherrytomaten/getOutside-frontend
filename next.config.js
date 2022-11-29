e3d34ffb13e94ea4953e69848faecbf417c8e5d5
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2022-11-29 20:48:23 +0100

    Update cypress tests

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  //disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
   dest: 'public',
   register: true,
   skipWaiting: true,
