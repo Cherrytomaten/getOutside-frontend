d23045e66ba6122ad658b2b86dcebb302e38c594
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2022-11-29 03:48:42 +0100

    update PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
+/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true
 })
 
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true
-};
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head'
 import { AuthProvider } from "@/context/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <>
+          <Head>
+              <meta charSet="utf-8" />
+              <meta name="description" content="Description" />
+              <meta name="keywords" content="Keywords" />
+              <title>GetOutside</title>
+
+              <link rel="manifest" href="/manifest.json" />
+              <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+              <meta name="theme-color" content="#3ED598" />
+          </Head>
+          <AuthProvider>
+              <Component {...pageProps} />
+          </AuthProvider>
+      </>
   );
 }
 
