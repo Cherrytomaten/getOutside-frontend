6b59c86bf0a54eba9389104da78288e2042c9595
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2023-01-05 14:12:30 +0100

    test PWA on deploy #2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development",
+  disable: false,
   dest: "public",
   register: true,
   skipWaiting: false,
