64099397ebe5cdf25126e8f1acfca74518d0a03b
Marlon <107611839+MarlonAACN@users.noreply.github.com>
2023-01-25 16:41:30 +0100

    Hotfix: API fix for get-data (#41)

--- src/pages/api/user/get-data.ts
+++ src/pages/api/user/get-data.ts
@@ -3,6 +3,8 @@ import axios, { AxiosResponse } from "axios";
 import { FetchServerErrorResponse } from "@/types/Server/FetchServerErrorResponse";
 import { BackendErrorResponse } from "@/types/Backend/BackendErrorResponse";
 import { UserDataProps } from "@/types/User/UserDataProps";
+import { AUTH_TOKEN } from "@/types/constants";
+import { TokenPayload } from "@/types/Auth/TokenPayloadProps";
 
 type UserDataResponseBody = {
     username: string;
@@ -25,20 +27,35 @@ export default async function handler(_req: NextApiRequest, res: UserDataRespons
         return res.status(405).json({errors: { message: 'Given request method is not allowed here.' } });
     }
 
-    return await axios.get('https://cherrytomaten.herokuapp.com/authentication/user/')
-        .then((_res: AxiosResponse<UserDataResponseBody>) => {
-            return res.status(201).json({
-                username: _res.data.username,
-                fname: _res.data.first_name,
-                lname: _res.data.last_name,
-                email: _res.data.email,
-                pfp: _res.data.profile_picture
-            });
-        })
-        .catch((err: BackendErrorResponse) => {
-            if (err.response?.data?.detail === undefined) {
-                return res.status(err.response.status).json({ errors: { message: "A server error occured." } });
+    try {
+        const authTokenString = _req.cookies[AUTH_TOKEN];
+        if (authTokenString === undefined || authTokenString === 'undefined') {
+            return res.status(400).json({errors: {message: 'Wrong token format.'}});
+        }
+
+        const authToken: TokenPayload = JSON.parse(authTokenString);
+
+        return await axios.get('https://cherrytomaten.herokuapp.com/authentication/user/', {
+            headers: {
+                'Authorization': 'Bearer ' + authToken.token
             }
-            return res.status(err.response.status).json({ errors: { message: err.response.data.detail } });
         })
+            .then((_res: AxiosResponse<UserDataResponseBody>) => {
+                return res.status(201).json({
+                    username: _res.data.username,
+                    fname: _res.data.first_name,
+                    lname: _res.data.last_name,
+                    email: _res.data.email,
+                    pfp: _res.data.profile_picture
+                });
+            })
+            .catch((err: BackendErrorResponse) => {
+                if (err.response?.data?.detail === undefined) {
+                    return res.status(err.response.status).json({ errors: { message: "A server error occured." } });
+                }
+                return res.status(err.response.status).json({ errors: { message: err.response.data.detail } });
+            })
+    } catch (_err) {
+        return res.status(400).json({errors: {message: 'Wrong token format.'}});
+    }
 }
